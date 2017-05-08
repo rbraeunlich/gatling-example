660dd7b7348963a443a1400b9b84265fd323505e
Ronny Bräunlich <ronny.braeunlich@codecentric.de>
2017-05-08 13:57:40 +0200

    create simple simulation

---
+++ src/test/scala/de/codecentric/gatling/example/SimpleSimulation.scala
@@ -0,0 +1,20 @@
+package de.codecentric.gatling.example
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+/**
+  * Created by ronny on 08.05.17.
+  */
+class SimpleSimulation extends Simulation {
+
+  private val httpConfig = http.baseURL("http://computer-database.gatling.io/computers")
+
+  private val scn = scenario("SimpleSimulation").
+    exec(http("open").
+      get("/")).
+    pause(1)
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpConfig)
+}
